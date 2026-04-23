7a23aca1011ee473f308a7a0deec7768b188a332
naosiro2-svg <naosiro2@gmail.com>
2026-04-23 16:31:42 +0900

    Update urls.js

--- urls.js
+++ urls.js
@@ -7,10 +7,10 @@
 const YOUTUBE_URLS = [
   ["", "", ""],  // 動画① [概要, 具体的内容, 実演]
   ["https://youtu.be/SfUdpyVwHvY", "", ""],  // 動画②
-  ["https://youtu.be/nU5BW-IayQI", "", ""],  // 動画③
-  ["", "", ""],  // 動画④
-  ["https://youtu.be/zO0385LzC9c", "", ""],  // 動画⑤
-  ["https://youtu.be/Y3UIv0OUDfE", "", ""],  // 動画⑥
+  ["https://youtu.be/nU5BW-IayQI", "https://youtu.be/6DqJeXa6sew", ""],  // 動画③
+  ["", "https://youtu.be/gkr2IuXNhN0", ""],  // 動画④
+  ["https://youtu.be/zO0385LzC9c", "https://youtu.be/xrtODLI-_cA", "https://youtu.be/Otg8wqC8fNE"],  // 動画⑤
+  ["https://youtu.be/Y3UIv0OUDfE", "", "https://youtu.be/KVxyq05t97c"],  // 動画⑥
   ["https://youtu.be/WQq7mYEZh6Y", "", ""],  // 動画⑦
 ];
 
