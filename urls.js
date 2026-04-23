6c6fd4a0756fef8437def782d568ad4b16024007
naosiro2-svg <naosiro2@gmail.com>
2026-04-23 15:39:26 +0900

    Update urls.js

--- urls.js
+++ urls.js
@@ -6,12 +6,12 @@
 
 const YOUTUBE_URLS = [
   ["", "", ""],  // 動画① [概要, 具体的内容, 実演]
-  ["", "", ""],  // 動画②
-  ["", "", ""],  // 動画③
+  ["https://youtu.be/SfUdpyVwHvY", "", ""],  // 動画②
+  ["https://youtu.be/nU5BW-IayQI", "", ""],  // 動画③
   ["", "", ""],  // 動画④
-  ["", "", ""],  // 動画⑤
-  ["", "", ""],  // 動画⑥
-  ["", "", ""],  // 動画⑦
+  ["https://youtu.be/zO0385LzC9c", "", ""],  // 動画⑤
+  ["https://youtu.be/Y3UIv0OUDfE", "", ""],  // 動画⑥
+  ["https://youtu.be/WQq7mYEZh6Y", "", ""],  // 動画⑦
 ];
 
 // 総合確認テスト GoogleフォームURL
