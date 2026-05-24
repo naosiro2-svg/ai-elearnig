59cf8efbf464b55f2723e72509dc3c81fb1816e7
naosiro2-svg <naosiro2@gmail.com>
2026-05-24 15:24:25 +0900

    Update urls.js

--- urls.js
+++ urls.js
@@ -8,7 +8,7 @@ const YOUTUBE_URLS = [
   ["https://youtu.be/fgqkvbxGeDY", "https://youtu.be/icMXtBDmgUU", ""],  // 動画① [概要, 具体的内容, 実演]　講座の目的とAIの基礎
   ["https://youtu.be/SfUdpyVwHvY", "https://youtu.be/pDoXGUrDxMI", "https://youtu.be/aqMCX7Ez8KM"],  // 動画②　AIプロンプト
   ["https://youtu.be/nU5BW-IayQI", "https://youtu.be/6DqJeXa6sew", ""],  // 動画③　ガイドライン
-  ["", "https://youtu.be/gkr2IuXNhN0", "https://youtu.be/CBp4u4AaCno"],  // 動画4 議事録　要約　調査　スライド作成
+  ["", "https://youtu.be/0GIoAAYamPs", "https://youtu.be/CBp4u4AaCno"],  // 動画4 議事録　要約　調査　スライド作成
   ["https://youtu.be/zO0385LzC9c", "https://youtu.be/xrtODLI-_cA", "https://youtu.be/QG2lwKB-tE0"],  // 動画⑤GEM
   ["https://youtu.be/Y3UIv0OUDfE", "", "https://youtu.be/LZvkrlupusg"],  // 動画⑥NOTEBOOKLM
   ["https://youtu.be/WQq7mYEZh6Y", "https://youtu.be/SEjopK-zJps", "https://youtu.be/K9RT4e0hbcE"],  // 動画⑦ビルド機能
